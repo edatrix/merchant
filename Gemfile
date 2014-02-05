8a5aeb0f24c4a0f427bdeb0ce2597b26b4d2f38e
edatrix <lespath@gmail.com>
2014-02-04 21:37:26 -0700

    adds better errors gem and creates orders

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ end
 
 group :development, :test do
   gem 'capybara'
+  gem 'better_errors'
+  gem 'binding_of_caller'
 end
 
 # Use ActiveModel has_secure_password
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,14 +6,10 @@ class Product < ActiveRecord::Base
       only_integer: true,
       greater_than_or_equal_to: 0
     }
-  # validates_inclusion_of :status, :in => ["unsubmitted", "needs_payment", "needs_packing", "needs_shipping", "shipped"]
 
   def price=(input)
     input.delete!("$")
     super
   end
-
-  # def self.status
-  # end
 
 end
