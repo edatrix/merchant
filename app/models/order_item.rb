a03734790174b25189c2ee2c9a4bf4685ae566ca
edatrix <lespath@gmail.com>
2014-02-04 22:42:22 -0700

    begin order calculations

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,6 +2,12 @@ class OrderItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product
 
-  validates :order_id, :product_id, presence: true
+  validates :order_id, presence: true
+  validates :product_id, presence: true
+  validates :quantity, numericality: { only_integer: true, greater_than: 0}
+
+  def subtotal(quantity, price)
+    quantity * price
+  end
 
 end
